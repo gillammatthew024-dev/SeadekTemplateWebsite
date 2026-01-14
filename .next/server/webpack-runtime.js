41f0641ecdac80eb0f977efe2fe9da030318869c
Matthew Gillam <gillammatthew024@gmail.com>
2026-01-14 03:51:04 -0500

    Change font sizing to be dynamic based on vw

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("fb63a77a0fbc3281")
+/******/ 		__webpack_require__.h = () => ("a20a378a5d5337f4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73ce264ab0bf239d"; }
+/******/ 		__webpack_require__.h = function() { return "de57c48f712c7da2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
